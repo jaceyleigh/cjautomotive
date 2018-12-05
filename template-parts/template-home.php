f0807c24b0001c347cad5b7d18a4fbfec7f1a1aa
Jacey Gulden <Jacey.Gulden@target.com>
2018-12-05 10:03:35 -0600

    committing all changes prior to app switch

---
+++ template-parts/template-home.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Template Name: Home
+ * @package cjautomotive
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area home-page">
+		<main id="main" class="site-main">
+
+      <main class="bd-masthead hero" id="content" role="main">
+        <div class="container">
+          <div class="row text-center pad-100">
+            <div class="col-12">
+              <h1>Honest, Trustworthy, Reliable Auto Care</h1>
+              <a href="#appointment">Schedule an Appointment</a>
+            </div>
+          </div>
+        </div>
+      </main>
+
+			<!-- 2 column Icon Grid -->
+			<section class="module_2 service-grid pad-100">
+				<div class="container">
+					<div class="row">
+						<div class="col-6 text-center">
+							<!-- top icons -->
+							<div class="row">
+								<div class="col-6">
+									<img src="http://placehold.it/100x100" />
+									<p>Search online for a realtor or talk to a family friend</p>
+								</div>
+								<div class="col-6">
+									<img src="http://placehold.it/100x100" />
+									<p>Have a conversation with a list of agents that all claim to be the best</p>
+								</div>
+							</div>
+							<!-- middle  icons -->
+							<div class="row">
+								<div class="col-6">
+									<img src="http://placehold.it/100x100" />
+									<p>Select one of the best agents based on a gut feeling</p>
+								</div>
+								<div class="col-6">
+									<img src="http://placehold.it/100x100" />
+										<p>Cross your fingers and hope for the best</p>
+								</div>
+							</div>
+							<!-- bottom  icons -->
+							<div class="row">
+								<div class="col-6">
+									<img src="http://placehold.it/100x100" />
+									<p>Select one of the best agents based on a gut feeling</p>
+								</div>
+								<div class="col-6">
+									<img src="http://placehold.it/100x100" />
+										<p>Cross your fingers and hope for the best</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!-- testimonials -->
+			<section class="testimonials grey-bg pad-100">
+				<div class="container">
+					<div class="row">
+						<div class="col-12 text-center">
+							<h3>Schedule an Appointment</h3>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
